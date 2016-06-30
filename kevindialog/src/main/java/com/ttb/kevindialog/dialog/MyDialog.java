6f63da584b10a9cc19049f5b63e9855b4c5561f0
demo124 <Kevin@ttbdeMacBook-Air.local>
2016-06-30 08:44:54 +0800

    compilite

--- kevindialog/src/main/java/com/ttb/kevindialog/dialog/MyDialog.java
+++ kevindialog/src/main/java/com/ttb/kevindialog/dialog/MyDialog.java
@@ -3,14 +3,20 @@ package com.ttb.kevindialog.dialog;
 import android.app.Dialog;
 import android.content.Context;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.TranslateAnimation;
 import android.widget.Button;
 
 import com.ttb.kevindialog.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Kevin on 16/6/29.
@@ -35,6 +41,11 @@ public abstract class MyDialog extends Dialog implements View.OnClickListener {
 
 	public MyDialog(Context context, int themeResId) {
 		super(context, themeResId);
+		this.context = context;
+		mMainLayout = LayoutInflater.from(context).inflate(themeResId, null);
+		createShowAnim();
+		createHideAnim();
+		getAllview();
 	}
 
 
@@ -90,5 +101,49 @@ public abstract class MyDialog extends Dialog implements View.OnClickListener {
 			dismiss();
 		}
 		return super.onKeyDown(keyCode, event);
+	}
+	public void setAttr(Context context,MyDialog dlg){
+		WindowManager.LayoutParams params = dlg.getWindow().getAttributes();
+		params.alpha = 0.8f; //窗口透明度
+		WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+		int width = wm.getDefaultDisplay().getWidth();
+		int height = wm.getDefaultDisplay().getHeight();
+		params.width =  width - 80;
+		params.height =  height / 4;
+		params.gravity = Gravity.CENTER_HORIZONTAL | Gravity.BOTTOM;
+		dlg.getWindow().setAttributes(params);
+		//必须设置一个背景，否则会有系统的Dialog样式：外部白框
+		dlg.getWindow().setBackgroundDrawableResource(R.drawable.kevin_translate);
+	}
+	public void getAllview(){
+		List<View> lst = getAllChildViews(mMainLayout);
+		for (View v:lst
+		     ) {
+			if(v instanceof Button)
+				v.setOnClickListener(this);
+		}
+	}
+	public static List<View> getAllChildViews(View view) {
+
+		List<View> allchildren = new ArrayList<>();
+
+		if (view instanceof ViewGroup) {
+
+			ViewGroup vp = (ViewGroup) view;
+
+			for (int i = 0; i < vp.getChildCount(); i++) {
+
+				View viewchild = vp.getChildAt(i);
+
+				allchildren.add(viewchild);
+
+				allchildren.addAll(getAllChildViews(viewchild));
+
+			}
+
+		}
+
+		return allchildren;
+
 	}
 }
